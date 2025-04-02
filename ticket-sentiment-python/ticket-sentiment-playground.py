1b9ab7968827775a490930f8a3f247d3324cdc5d
Marco Nätlitz <432346+marconae@users.noreply.github.com>
2025-04-02 20:31:26 +0200

    Added two runs for speed comparison

--- ticket-sentiment-python/ticket-sentiment-playground.py
+++ ticket-sentiment-python/ticket-sentiment-playground.py
@@ -1,11 +1,26 @@
 from transformers import pipeline
+import time
+
+PIPE = pipeline("text-classification", model="cardiffnlp/twitter-roberta-base-sentiment-latest") # 150 MB
 
 def sentiment(text:str):
-    pipe = pipeline("text-classification", model="cardiffnlp/twitter-roberta-base-sentiment-latest") # 150 MB
-    return pipe(text)
+    return PIPE(text)
 
 if __name__ == "__main__":
     ticket_text = "I'm having an issue with the Bose Sound Link. Please assist. This problem started occurring after the recent software update. I haven't made any other changes to the device."
-    print(sentiment(ticket_text))
 
-    #[{'label': 'negative', 'score': 0.8280922770500183}]
+    start_time = time.perf_counter()
+    sentiment1 = sentiment(ticket_text)
+    end_time = time.perf_counter()
+
+    print(f"Sentiment 1: {sentiment1}")
+    print(f"Execution Time: {end_time - start_time}")
+
+    start_time = time.perf_counter()
+    sentiment2 = sentiment(ticket_text)
+    end_time = time.perf_counter()
+
+    print(f"Sentiment 2: {sentiment2}")
+    print(f"Execution Time: {end_time - start_time}")
+
+
